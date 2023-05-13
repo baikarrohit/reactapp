7fcff63d38c8e97de694cd6a0adf32a871ed823b
baikarrohit <baikarrohit1999@gmail.com>
2023-05-13 13:11:15 +0530

    Added click events "change title and delete"

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -4,11 +4,18 @@ import './Expensedetails';
 import ExpenseDetails from './Expensedetails';
 
 const ExpenseItem = (props) => {
-   
+   const clickHandler = () => {
+    console.log("Clicked!!!");
+   };
+   const expenseDeletd = () => {
+    console.log("Deleted!");
+   };
     return (
         <div className="expense-item"> 
             <ExpenseDate date={props.date}/>
             <ExpenseDetails title={props.title} expenditure={props.expenditure} amount={props.amount}/>
+            <button onClick={clickHandler}>Change Title</button>
+            <button onClick={expenseDeletd}>Delete Expense</button>
         </div>
         );
 }
