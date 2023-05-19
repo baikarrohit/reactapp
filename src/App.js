bad8fba66c7d5ab2bb569f2a9ba042a6b0c81ff4
baikarrohit <baikarrohit1999@gmail.com>
2023-05-19 15:58:02 +0530

    added unique keys to map function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 //import ExpenseItem from './components/Expenses/Expenseitem';
+import React,{useState} from "react";
 import NewExpense from "./components/NewExpenses/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 import "./App.css";
 
-const App = (props) => {
-  
+let dummyExpenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2022, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2022, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.44,
+    date: new Date(2023, 4, 14),
+  },
+  {
+    id: "e4",
+    title: "New Desk(Wooden)",
+    amount: 405,
+    date: new Date(2023, 3, 10),
+  },
+];
 
-  const addExpenseHandler = (expense) => {
-    const ExpenseData = {
-      ...expense,
-      id:Math.random().toString()
-    };
-    console.log(ExpenseData);
+const App = () => {
+    const [expenses,setExpenses] = useState(dummyExpenses);
+
+    const addExpenseHandler = (expense) => {
+      setExpenses((prevExpenses) => {
+        return [expense, ...prevExpenses];
+      });
+  
   }
   return (
     <div className="App">
       <h2>Lets get started with React!</h2>
 
       <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses />
+      <Expenses items={expenses} />
       
     </div>
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,53 +10,19 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
-    let expenses = [
-        {
-          id: "e1",
-          title: "Toilet Paper",
-          LocationOfExpenditure: 'Home',
-          amount: 94.12,
-          date: new Date(2022, 7, 14),
-        },
-        {
-          id: "e2",
-          title: "New TV",
-          LocationOfExpenditure: 'Home',
-          amount: 799.49,
-          date: new Date(2022, 2, 12),
-        },
-        {
-          id: "e3",
-          title: "Car Insurance",
-          LocationOfExpenditure: 'Car',
-          amount: 294.44,
-          date: new Date(2023, 4, 14),
-        },
-        {
-          id: "e4",
-          title: "New Desk(Wooden)",
-          LocationOfExpenditure: 'Home',
-          amount: 405,
-          date: new Date(2023, 3, 10),
-        },
-      ];
-
+    
   return (
     <div>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
 
-        {expenses.map((ele)=>{
-        return (
-            <div key={ele.title}> 
-                <ExpenseItem 
-                    title={ele.title}
-                    expenditure={ele.LocationOfExpenditure}
-                    amount={ele.amount}
-                    date={ele.date}
-                /> 
-            </div>)
-        
-        })}
+        {props.items.map((expense)=>(
+            <ExpenseItem 
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            /> 
+        ))}
     </div>
     
   );
