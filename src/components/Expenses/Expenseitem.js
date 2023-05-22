c71551ec9a1cd8ef5c3267e2f6e77b60f7927aef
baikarrohit <baikarrohit1999@gmail.com>
2023-05-22 15:15:53 +0530

    added conditional return statment

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -1,34 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './Expensedate';
 import './Expenseitem.css';
-import './Expensedetails';
 import ExpenseDetails from './Expensedetails';
 
 
 const ExpenseItem = (props) => {
-   const [title,setTitle] = useState(props.title);
-
-   const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-   };
-   /*const expenseDeleted = () => {
-    console.log("Deleted!");
-   };*/
-   const [expense,setExpense] = useState(props.amount);
-   const changeExpense = () => {
-    setExpense("100$");
-   }
+   
     return (
-        <div className="expense-item"> 
-            <ExpenseDate date={props.date}/>
-            <ExpenseDetails title = {title} expenditure = {props.expenditure} amount={expense}/>
-            <button onClick={clickHandler}>Change Title</button>
-            {/*<button onClick={expenseDeleted}>Delete Expense</button>*/}
-            <button onClick={changeExpense}>Change Expense</button>
-
-        </div>
-        );
+        <li>
+            <div className="expense-item"> 
+                <ExpenseDate date={props.date}/>
+                <ExpenseDetails title = {props.title} amount={props.amount}/>
+            </div>
+        </li>
+    );
 }
 
 export default ExpenseItem;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,6 @@
 import React, {useState} from 'react';
 import ExpensesFilter from './Expensefilter';
-import ExpenseItem from './Expenseitem';
-
-//import './Expenses.css';
+import ExpensesList from './Expenseslist';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -14,19 +12,13 @@ const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
 }); 
 
-    
+
+
   return (
     <div>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-
-        {filteredExpenses.map((expense)=>(
-            <ExpenseItem 
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            /> 
-        ))}
+        <ExpensesList items={filteredExpenses}/>
+        
     </div>
     
   );
